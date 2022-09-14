2f6659143fe445eaf8559e35c9097370151b4a1e
Gauthier Rotsart de Hertaing <gauthier.rotsart@uclouvain.be>
2022-09-14 14:57:08 +0200

    script generation for RQ2

--- metrics/metrics_evaluation.py
+++ metrics/metrics_evaluation.py
@@ -9,62 +9,127 @@ import numpy as np
 from skimage.measure import shannon_entropy as entropy
 import cv2
 import math 
+from sklearn.metrics import mutual_info_score
 
 
+def temporalAverage(subsetIndices,imageList,shape):
+    matrix = np.zeros((len(subsetIndices),len(subsetIndices)))
+    for i in range(len(subsetIndices)):
+        for j in range(len(subsetIndices)):
+            if i < j:
+                matrix[i][j] = abs(subsetIndices[i]-subsetIndices[j])
+                
+    timeInterval = []
+    for i in range(len(subsetIndices)-1):
+        if i < len(subsetIndices):
+            dt = subsetIndices[i+1]-subsetIndices[i]
+            timeInterval.append(dt)
+
+    mu = np.mean(timeInterval)  
+    listNeighbours = []
+    for i in range(len(subsetIndices)):
+        li = []
+        for j in range(len(subsetIndices)):
+            if i < j and matrix[i][j] <= mu:
+                if li == []:
+                    li = [subsetIndices[i]]
+                li.append(subsetIndices[j])
+        listNeighbours.append(li)
+                
+    l = listNeighbours
+    l2 = l[:]
+    for m in l:
+        for n in l:
+            
+            if set(m).issubset(set(n)) and m != n:
+                l2.remove(m)
+                break
+            
+    l2 = list(set(map(tuple,l2)))
+    averagedListImages = []
+    for indices in l2:
+        meanImage = np.zeros(shape)
+        for k in indices:
+            meanImage += imageList[subsetIndices.index(k)]
+        
+        meanImage /= len(indices)
+        averagedListImages.append(meanImage)
+    return averagedListImages
+
+def calc_MI(image1, image2, bins):
+    c_xy = np.histogram2d(image1, image2, bins)[0]
+    mi = mutual_info_score(None, None, contingency=c_xy)
+    return mi
+    
 def snr(a, axis=0, ddof=0):
     a = np.asanyarray(a)
     m = a.mean(axis)
     sd = a.std(axis=axis, ddof=ddof)
-    return np.where(sd == 0, 0, m/sd).tolist()
+    return np.mean(np.where(sd == 0, 0, m/sd).tolist())
 
-#compute the individual entropy for each frame
-def indEntropy(imageList):
-    individualEntropy = []
-    for image in imageList:
-        individualEntropy.append(entropy(image))
-        
-    meanEntropy = np.mean(individualEntropy)
-    resultList = individualEntropy#[meanEntropy]
-    return resultList
-
-def indSNR(imageList):
-    individualSNR = []
-    for image in imageList:
-        individualSNR.append(snr(image,axis=None))
-    
-    meanSNR = np.mean(individualSNR)
-    resultList = individualSNR#[meanSNR]
-    return resultList
 
 #return the metric's score for each subsampling method
-def evaluation(imageFolder,subsetIndices,metricsChoice):
-    scoreList = []
+def evaluationIntraFrame(imageFolder,subsetIndices,metricsChoice,shape):
+    individualEntropy = []
+    individualSNR = []
+    individualEntropy_bg = []
+    individualSNR_bg = []
+    subsetSize = len(subsetIndices[0])#suppose a constant number of subset frames
+    for idxList in subsetIndices:#loop for each subsampling method
+        #open images and compute the background
+        background = np.zeros(shape)
+        for idx_img in idxList:
+            frame_folder = "frame_" + str(idx_img).zfill(4) + ".png"
+            im = plt.imread(imageFolder + frame_folder)
+            im = np.floor((0.2126*im[:,:,0]+0.7152*im[:,:,1]+0.0722*im[:,:,2])*255)
+            background += im
+            individualEntropy.append(entropy(im))
+            individualSNR.append(snr(im))
+            #imageList.append(im)
+        background /= subsetSize
+        
+        for idx_img in idxList:
+            frame_folder = "frame_" + str(idx_img).zfill(4) + ".png"
+            im = plt.imread(imageFolder + frame_folder)
+            im = np.floor((0.2126*im[:,:,0]+0.7152*im[:,:,1]+0.0722*im[:,:,2])*255)
+            im = (im + 255 - background)//2
+            individualEntropy_bg.append(entropy(im))
+            individualSNR_bg.append(snr(im))
+    return individualEntropy,individualEntropy_bg,individualSNR,individualSNR_bg
+
+def evaluationInterFrame(imageFolder,subsetIndices,metricsChoice,shape):
+    cst = math.log(math.exp(1),2)
+    miScore = []
+    psnrScore = []
     subsetSize = len(subsetIndices[0])#suppose a constant number of subset frames
     for idxList in subsetIndices:#loop for each subsampling method
         #open images and compute the background
         imageList = []
-        background = np.zeros((1920,2560))
+        background = np.zeros(shape)
         for idx_img in idxList:
-            frame_folder = "frame_" + str(idx_img) + ".png"
+            frame_folder = "frame_" + str(idx_img).zfill(4) + ".png"
             im = plt.imread(imageFolder + frame_folder)
             im = np.floor((0.2126*im[:,:,0]+0.7152*im[:,:,1]+0.0722*im[:,:,2])*255)
             background += im
             imageList.append(im)
         background /= subsetSize
         
-        individualScore = [indEntropy(imageList), indSNR(imageList)]
-        scoreList.append(individualScore)
-        
-        image1 = imageList[0]
-        image2 = imageList[1::]
-        
-        averagingMethod(image1,image2)
-        
+        temporalAvImages = temporalAverage(idxList,imageList,shape)
+        for i in range(len(temporalAvImages)):
+            image1 = temporalAvImages[i]
+            image2 = temporalAvImages[0:i] + temporalAvImages[i+1::]
+            meanImage = np.zeros(shape)
+            for j in range(len(image2)):
+                meanImage += image2[j]
+            meanImage /= len(image2)
+            mi = calc_MI(image1.flatten(), meanImage.flatten(), 256)
+            psnr = cv2.PSNR(image1, meanImage)
+            miScore.append(mi*cst)
+            psnrScore.append(psnr)
+        """
         #remove background for each frame and then, compute the metric's score
         for j in range(subsetSize):
             imageList[j] = (imageList[j] + 255 - background)//2
+        """
         
-        individualScore_bg = [indEntropy(imageList), indSNR(imageList)]
-        scoreList.append(individualScore_bg)
-    return scoreList
-
+    return miScore,psnrScore
---
+++ metrics/metrics_txt_generation.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  9 13:39:43 2022
+
+@author: grotsartdehe
+"""
+
+#for the script
+import matplotlib.pyplot as plt 
+import random
+import time 
+import numpy as np
+import os
+from TrailRQ2 import evaluationIntraFrame,evaluationInterFrame
+
+def writeCSV(filename,cameraFolder,metric):
+    with open("E:/Data_trail/metrics/"+cameraFolder+filename, 'w') as file_handler:
+        for item in metric:
+            file_handler.write("{}\n".format(item))
+def main():
+    startTime = time.time()
+    cameraFolder = 'C024/'
+    imageFolder = "E:/Data_trail/S05c024/bank/images/"
+    frame_folder = "frame_" + str(0).zfill(4) + ".png"
+    im = plt.imread(imageFolder + frame_folder)
+    H,W,_ = im.shape
+    intraframe = False
+    if intraframe:
+        lst = os.listdir(imageFolder) # your directory path
+        number_files = len(lst)
+        idxList = [i for i in range(0,number_files)]#[i for i in range(3730, 3730+30)]
+        indexList = [idxList]#len corresponds to number of strategies
+        
+        metricsChoice = ['Entropy']
+        
+        individualEntropy,individualEntropy_bg,individualSNR,individualSNR_bg = evaluationIntraFrame(imageFolder,indexList,metricsChoice,(H,W))
+        writeCSV("entropy.txt",cameraFolder, individualEntropy)
+        writeCSV("entropy_bg.txt",cameraFolder, individualEntropy_bg)
+        writeCSV("snr.txt",cameraFolder, individualSNR)
+        writeCSV("snr_bg.txt",cameraFolder, individualSNR_bg)
+    else:
+        LogFolder = "E:/Data_trail/runs/"+cameraFolder
+        LogFolder_list = os.listdir(LogFolder)
+        
+        for i in range(0,len(LogFolder_list)):
+            print(LogFolder_list[i])
+            with open(LogFolder + LogFolder_list[i],'r') as f:
+                mylist = f.readlines()
+            idxList = []
+            log = LogFolder_list[i].split('.')[0]
+            log_split = log.split('-')
+            nbrFrames = log_split[2]
+            strat = log_split[4]
+            for j in range(len(mylist)):
+                line = mylist[j].split('/')
+                line = line[-1].split('.')
+                line = line[0].split('_')
+                idx = line[-1]
+                idxList.append(int(idx))
+    
+            idxList.sort()   
+            indexList = [idxList]
+            metricsChoice = ['Entropy']
+            miScore,psnrScore = evaluationInterFrame(imageFolder,indexList,metricsChoice,(H,W))
+            writeCSV(LogFolder_list[i].split('.')[0]+'mutual_information.txt',cameraFolder, miScore)
+            writeCSV(LogFolder_list[i].split('.')[0]+'psnr.txt',cameraFolder, psnrScore)
+    
+    endTime= time.time()
+    print("Execution time: ", endTime - startTime)
+    #return scores,miScore,psnrScore#indEntropy_mean,indSNR_mean,indEntropy_bg_mean,indSNR_bg_mean, mi_mean,psnr_mean
+"""
+nbrFrames = [10,25,50,75,100]
+indEntropy_mean,indSNR_mean,indEntropy_bg_mean,indSNR_bg_mean,mi_mean,psnr_mean = main()
+plt.figure()
+plt.scatter(nbrFrames,indEntropy_mean)
+plt.title("Entropy with background")
+
+plt.figure()
+plt.scatter(nbrFrames,indEntropy_bg_mean)
+plt.title("Entropy without background")
+
+plt.figure()
+plt.scatter(nbrFrames,indSNR_mean)
+plt.title("SNR with background")
+
+plt.figure()
+plt.scatter(nbrFrames,indSNR_bg_mean)
+plt.title("SNR without background")
+
+plt.figure()
+plt.scatter(nbrFrames,mi_mean)
+plt.title("Mutual information")
+
+plt.figure()
+plt.scatter(nbrFrames,psnr_mean)
+plt.title("psnr")
+"""
+"""
+scores,miScore,psnrScore = main()
+withBG = scores[0]
+indEntropy = withBG[0]
+snr = withBG[1]
+withoutBG = scores[1]
+indEntropy_bg = withoutBG[0]
+snr_bg = withoutBG[1]
+
+plt.figure()
+plt.plot(indEntropy)
+plt.plot(indEntropy_bg)
+plt.title("Entropy")
+plt.legend(["With background", "Without background"])
+
+plt.figure()
+plt.plot(snr)
+plt.plot(snr_bg)
+plt.title("SNR")
+plt.legend(["With background", "Without background"])
+
+plt.figure()
+plt.plot(miScore)
+plt.title("Mutual information")
+
+plt.figure()
+plt.plot(psnrScore)
+plt.title("Psnr")
+"""
+main()
