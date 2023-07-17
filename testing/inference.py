638268e70df07c685cd5f3dbb9df7f2307a45caa
Dani Manjah <manjahdani@gmail.com>
2023-07-17 10:55:52 +0200

    Improvement of the inference.py

--- testing/inference.py
+++ testing/inference.py
@@ -1,14 +1,14 @@
-import sys
-import os
+import argparse
 import csv
 import cv2
-import json
-import argparse
-from tabulate import tabulate
-import time
-import logging
-
 import gc
+import json
+import logging
+import os
+import pandas as pd
+import sys
+import time
+from tabulate import tabulate
 import torch
 
 if __name__ == '__main__':
@@ -18,28 +18,27 @@ elif __name__ == 'testing.inference':
     sys.path.append(os.path.join(sys.path[0], "yolov8", "ultralytics"))
     from ultralytics import YOLO
 
+# NAMING CONVENTION:
+# {Source_domain}_{Target_domain}_{Student}_{Teacher}_{Strategy}_{Active-Learning-Setting}_{Total_Samples}
+# Finer-granularity can be expected as
+# {Source_domain} = {dataset}-{domain}-{period}
+#
+# The use of _ is not allowed in any of the subcomponents. E.g. Strategy = 'least_confidence_max' is wrong. Instead, one should have 'least-confidence-max'
+#
+# In the case of multi-run, we assume a balanced class therefore there is only need to have the name of the domain and their respective period as
+# domain = domain{1,2,3,n}; E.g., if we trained on cam 1,11,120 and used the 1st week for cam 1, the second and fourth week for cam 11 and 120 then it should be "cam1,11,120", "week1,2,4".
+#
+# run = 2kq3iot2.WALT-cam3,4-week5,6_yolov8n_yolov8x6_least-confidence-max_stream-based_16
+# run_name = WALT-cam3,4-week5,6_yolov8n_yolov8x6_least-confidence-max_stream-based_16
 
 BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 TMP_DATA_YAML = os.path.join(BASE_PATH, 'data.yaml')
 
-STRATEGIES = ['last_25_random','last_50_random', # RANDOM MULTI-ITERATION
-               'frequency_50_confidence_max', 'frequency_75_confidence_max','frequency_100_confidence_max',
-               "threshconfidentTeacherStreambased_max",
-               'frequency_50_confidence_min','frequency_75_confidence_min','frequency_100_confidence_min',
-               'frequency_50_topconfidence_max', 'frequency_75_topconfidence_max','frequency_100_topconfidence_max',
-               'n_first', 'fixed_interval', 'flow_diff', 'flow_interval_mix', 'entropy', 'frequency',
-               'random0','random1','random1', 'random2', 'random3', 'randomunknown',
-               "topconfidence_max","topconfidence_sum","movement",
-               "uniformStreambased", #Random stream-based
-               "threshTopconfidenceStreambased_sum", "threshTopconfidenceStreambased_max", "threshLeastconfidenceStreambased_max", #stream-based
-               "bernoulliLeastconfidenceStreambased_max","bernoulliTopconfidenceStreambased_max","confidentTeacher_max",
-               "confidence_max", "confidence_min"]
-# METRICS = ['metrics/precision(B)', 'metrics/recall(B)', 'metrics/mAP50(B)', 'metrics/mAP50-95(B)', 'fitness']
 METRICS = ['precision', 'recall', 'mAP50', 'mAP50-95', 'fitness']
 
 def get_runs_summary(weights_path, project, wandb_project_name):
     summary = os.path.join(os.path.abspath(weights_path), wandb_project_name + '.csv')
-    with open(summary, 'r', encoding = 'utf-8') as f:
+    with open(summary, 'r', encoding='utf-8') as f:
         lines = f.readlines()[1:]
 
     summary_processed = {}
@@ -50,7 +49,7 @@ def get_runs_summary(weights_path, project, wandb_project_name):
             summary_processed[run_name] = run_summary
         except Exception as e:
             print("ERROR, Fix this first --->", line)
-            logging.exception(e) 
+            logging.exception(e)
             sys.exit()
         continue
 
@@ -62,34 +61,40 @@ def get_weights(weights_path, project):
     weights = [os.path.join(os.path.abspath(weights_path), w) for w in weights if w.endswith('.pt') and project in w]
     return weights
 
+
 def build_run_info(weight, dataset_path, project, summary):
     run = weight.split('/')[-1]
     run_name = run.split('.')[1]
-    print(project)
-    for strategy in STRATEGIES:
-        if strategy in run.split(project)[1]:
-            break
     if run_name in summary:
-        print('run_name',run_name)
-        # '-'.join(run.split(project)[1].strip('-').split('_')[0].split('-')),
-        teacher = run_name.split('_')[-1]
+        parts = run_name.split("_")
+        
         return {
             'id': run.split('.')[0],
-            # 'data-name': run.split(project)[1].strip('-').split('_')[0].split('-')[0],
-            'data-name': '-'.join(run.split(project)[1].strip('-').split('_')[0].split('-')),
-            'strategy': strategy,
+            'data-name': run.split(project)[1].strip('-').split('_')[0].split('-')[0],
+            'source_dataset':parts[0].split("-")[0],
+            'source_domain': parts[0].split("-")[1],
+            'source_period': parts[0].split("-")[2],
+            'student' : parts[1],
+            'teacher':  parts[2],
+            'strategy': parts[3],
             'epochs': summary[run_name]['_step'],
             'best/epoch': summary[run_name]['best/epoch'],
-            'samples': int(run.split(project)[1].split(strategy)[1].strip('_').split('.')[0].split('-')[0].split('_')[0]),
-            'data': os.path.join(dataset_path, run.split(project)[1].strip('-').split('_')[0].split('-')[0]), # *run.split(project)[1].strip('-').split('_')[0].split('-')
-            'teacher': teacher  # Add this line
+            'setting':parts[4],
+            'data':os.path.join(dataset_path, parts[0].split("-")[1]), #test_set path
+            'samples': int(parts[5]),
         }
 
-def main(weights_path, csv_path, dataset_path, project, wandb_project_name, base_data_yaml, task):
+def main(weights_path, csv_path, dataset_path, project, wandb_project_name, base_data_yaml, task, target_domains):
+    # Check if GPU is available
+    if torch.cuda.is_available():
+        device = "cuda:0"  # Use GPU
+    else:
+        device = None  # Use CPU
+    torch.cuda.set_device(device)
     weights = get_weights(weights_path, project)
     summary = get_runs_summary(weights_path, project, wandb_project_name)
-
     runs = []
+    
     for weight in weights:
         info = build_run_info(weight, dataset_path, project, summary)
         if info:
@@ -97,51 +102,61 @@ def main(weights_path, csv_path, dataset_path, project, wandb_project_name, base
         
     if not runs:
         print('No valid weights/data were found for testing')
-        return
-
+        return []
     if not os.path.isfile(csv_path):
         with open(csv_path, 'w') as f:
             writer = csv.writer(f)
-            writer.writerow(['run_id', 'data-name', 'strategy', 'teacher', 'epochs', 'best/epoch', 'samples', *METRICS])
+            writer.writerow(['run_id', 
+                            'source_dataset', 'source_domain','source_period',
+                            'target_domain', #@FIXME Should include target_dataset and target_period
+                            'student', 'teacher', 
+                            'epochs', 'best/epoch', 
+                            'strategy',
+                            'setting', 
+                            'samples', 
+                            *METRICS])
+    df=pd.read_csv(csv_path)
 
-    testable = 0
-    with open(csv_path, 'r') as f:
-        lines = f.readlines()[1:]
-        for run in runs:
-            run['tested'] = 'NO'
-            for line in lines:
-                if run['id'] in line:
-                    run['tested'] = 'YES'
-                    testable += 1
 
-    print(f'Found {len(runs)} runs for project {project} of which {testable} need testing')
+    print(f'Found {len(runs)} runs for project {project} need testing')
    
-    print(tabulate([r.values() for r in runs], headers=['RUN-ID', 'DATA-SHORT-NAME', 'STRATEGY', 'TEACHER', 'EPOCHS', 'BEST/EPOCH', 'SAMPLES', 'DATA', 'MODEL', 'TESTED']))
-    
-    # testing
     for i, run in enumerate(runs):
         try:
-            if run['tested'] == 'YES':
-                print(f"Run {run['id']}:{run['data-name']} has already been tested.............Done")
-                continue
-            
             print(f'[{i+1}/{len(runs)}] Testing... : {run["id"]}')
-            build_yaml_file(base_data_yaml, run['data'])
-            model = YOLO(run['model'])
-            results = model.val(data=TMP_DATA_YAML, task=task)
             
-            if len(results) == len(METRICS):
-                with open(csv_path, 'a+') as f:
-                    writer = csv.writer(f)
-                    writer.writerow([run['id'], run['data-name'], run['strategy'], run['teacher'], run['epochs'], run['best/epoch'], run['samples'], *list(results.values())])
-            else:
-                print('TESTING ERROR. NOT SAVING !')
+            for target_domain in target_domains:
+                if run['id'] in df['run_id'].values:
+                    filtered_df = df[df['run_id'] == run['id']]
+                    if target_domain in filtered_df['target_domain'].values:
+                        print(f"Run {run['id']}:{run['data-name']} has already been tested on {target_domain}.............Done")
+                        continue
+                    
+                target_domain_path = os.path.join(dataset_path,target_domain)
+                build_yaml_file(base_data_yaml, target_domain_path)
+                model = YOLO(run['model'])
+                results = model.val(data=TMP_DATA_YAML, task=task,device=device)
+                
+                if len(results) == len(METRICS):
+                    with open(csv_path, 'a+') as f:
+                        writer = csv.writer(f)
+                        writer.writerow([run['id'], 
+                                            run['source_dataset'], run['source_domain'], run['source_period'], 
+                                            target_domain, 
+                                            run['student'],run['teacher'], 
+                                            run['epochs'],
+                                            run['best/epoch'], 
+                                            run['strategy'],
+                                            run['setting'], 
+                                            run['samples'], 
+                                            *list(results.values())])
+                else:
+                    print('TESTING ERROR. NOT SAVING !')
 
-            print(f"Run {run['id']}:{run['data-name']} just finished being tested..............Done")
-            print('Seelping and clearing memory')
-            time.sleep(1)
-            torch.cuda.empty_cache()
-            gc.collect()
+                print(f"Run {run['id']}:{run['data-name']} on target domain {target_domain} just finished being tested..............Done")
+                print('Sleeping and clearing memory')
+                time.sleep(1)
+                torch.cuda.empty_cache()
+                gc.collect()
         except AssertionError as e:
             print(e)
         except Exception as e:
@@ -149,9 +164,9 @@ def main(weights_path, csv_path, dataset_path, project, wandb_project_name, base
 
     return runs
 
-
-
 def build_yaml_file(base_file, dataset):
+    print(base_file)
+    print('dataset',dataset)
     lines_to_write = []
     with open(base_file, 'r') as f:
         lines = f.readlines()
@@ -167,24 +182,26 @@ def build_yaml_file(base_file, dataset):
 if __name__ == "__main__":
     ap = argparse.ArgumentParser()
 
-    ap.add_argument('-w', '--weights_path', type=str, required=True,
+    ap.add_argument('-w', '--weight_path', type=str, required=True,
                     help='The path to the weights to be evaluated')
-    ap.add_argument('-c', '--csv_path', type=str, required=False,
-                    help='The path to the CSV with the results')
     ap.add_argument('-d', '--dataset_path', type=str, required=True,
                     help='The path to the dataset folder, it should contain each camera in a separated folder')
     ap.add_argument('-p', '--project', type=str, required=True,
                     help='This is not the project name used in wandb, this is the dataset name used as prefix')
-    ap.add_argument('-p', '--wandb_project', type=str, required=False,
+    ap.add_argument('-wp', '--wandb_project', type=str, required=False,
                     help='This is the project name used in wandb')
-    ap.add_argument('-y', '--data-template', type=str, required=True,
+    ap.add_argument('-y', '--data_template', type=str, required=True,
                     help='Template yaml file for the dataset')
     ap.add_argument('-f', '--folder', type=str, required=False, default='test',
                     help='Set the folder to be used for testing: val or test')
+    ap.add_argument('-td', '--target_domains', type=str, required=True,
+                    help='Set the target_domains to be used for testing')
     args = ap.parse_args()
 
     if not args.wandb_project:
         args.wandb_project = args.project
 
-    print(args.data_template)
-    #main(args.weights_path, args.csv_path, args.dataset_path, args.project, args.wandb_project, args.data_template, args.folder)
+    
+    main(args.weight_path, args.dataset_path, args.project, args.wandb_project, args.data_template, args.folder, args.target_domains.split(','))
+
+
